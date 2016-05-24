47eebbdd0ff770134b8b3cf1bf9893831b2a6b58
Jyri-Matti Lähteenmäki <jyri-matti@lahteenmaki.net>
2016-05-24 23:22:20 +0300

    updated urls

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 var config = function() {
     return {
         tkl: {
-            url: 'http://lahteenmaki.net/xdm?http://api.publictransport.tampere.fi/1_0_2/',
+            url: '/bus-tre/',
             username: 'businfo',
             password: 'businfo'
         },
         hsl: {
-            url: 'http://lahteenmaki.net/xdm?http://api.reittiopas.fi/hsl/prod/',
+            url: '/bus-hsl/',
             username: 'businfo',
             password: 'businfo'
         },
         siri: {
             application_id: 'businfo',
-            url: 'http://lahteenmaki.net/xdm?https://siri.ij2010.tampere.fi/ws',
+            url: '/bus-siri/',
             username: 'its_factory_temp',
             password: 'ITS4devN'
         },
         siriRT: {
-            url: 'http://lahteenmaki.net/xdm?http://data.itsfactory.fi/siriaccess/vm/json'
+            url: '/bus-json/'
         },
         tampereCoordinates: {latitude: 61.4981, longitude: 23.760},
         helsinkiCoordinates: {latitude: 60.1675763, longitude: 24.9417421},
